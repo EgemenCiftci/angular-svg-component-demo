d1458bcdf8d0a6c8c145f5a0d000a7320e6911a3
EgemenCiftci <egemenciftci@gmail.com>
2022-04-02 08:43:08 +0200

    1 file modified

--- src/app/chart/chart.component.ts
+++ src/app/chart/chart.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
-import { ShapeInfo } from '../app.component';
+import { ShapeInfo } from '../shape-info';
 
 @Component({
   selector: 'app-chart',
